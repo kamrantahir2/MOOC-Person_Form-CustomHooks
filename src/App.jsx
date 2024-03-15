6b562920590dff2b11d32eefba288d6500dc79b5
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-03-15 20:07:53 +0000

    Build person components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+  const [height, setHeight] = useState("");
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form>
+          name:
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <br />
+          birthdate:
+          <input
+            type="date"
+            value={born}
+            onChange={(event) => setBorn(event.target.value)}
+          />
+          <br />
+          height:
+          <input
+            type="number"
+            value={height}
+            onChange={(event) => setHeight(event.target.value)}
+          />
+        </form>
+        <div>
+          {name} {born} {height}{" "}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
